a6a66174607134ddfe42bf05c84252a5bcf43c9d
ponnamallasuvarna <99868683+ponnamallasuvarna@users.noreply.github.com>
2023-02-02 04:10:25 -0500

    n=int(input()) temp=n sum=0 product=1 while(n!=0):     r=n%10     sum=sum+r     product=product*r     n=n//10 if sum==product:     print("Spy Number") else:     print ("Not Spy Number")

---
+++ Spy_Number.py
@@ -0,0 +1,13 @@
+n=int(input())
+temp=n
+sum=0
+product=1
+while(n!=0):
+    r=n%10
+    sum=sum+r
+    product=product*r
+    n=n//10
+if sum==product:
+    print("Spy Number")
+else:
+    print ("Not Spy Number")
