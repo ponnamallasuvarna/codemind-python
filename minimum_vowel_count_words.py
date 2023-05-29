d371cb8dcf22a45b1b9261ca18d7efdaf1bcddba
ponnamallasuvarna <99868683+ponnamallasuvarna@users.noreply.github.com>
2023-05-29 05:14:17 -0400

    def count(s):     a="aeiou"     c=0     for i in s:         if i in a:             c=c+1     return c n=input().split() lst=list(n) p=[] l=[] for i in lst:     p.append(count(i)) q=min(p) for i in p:     print(p.count(q))     break

---
+++ minimum_vowel_count_words.py
@@ -0,0 +1,17 @@
+def count(s):
+    a="aeiou"
+    c=0
+    for i in s:
+        if i in a:
+            c=c+1
+    return c
+n=input().split()
+lst=list(n)
+p=[]
+l=[]
+for i in lst:
+    p.append(count(i))
+q=min(p)
+for i in p:
+    print(p.count(q))
+    break
