0b6649d29ae4428b770bbf0d1ccfe1910e8ff8c9
ponnamallasuvarna <99868683+ponnamallasuvarna@users.noreply.github.com>
2023-06-13 04:32:11 -0400

    m=int(input()) l=[] for i in range(1,m+1):     l.append(i) s=[str(integer)for integer in l] a_string="".join(s) n=int(a_string) p=str(n) q=len(p) while n:     r=n%10**q     print(r)     n=n//10

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,13 @@
-n=int(input())
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end="")
-    print()
+m=int(input())
+l=[]
+for i in range(1,m+1):
+    l.append(i)
+s=[str(integer)for integer in l]
+a_string="".join(s)
+n=int(a_string)
+p=str(n)
+q=len(p)
+while n:
+    r=n%10**q
+    print(r)
+    n=n//10
